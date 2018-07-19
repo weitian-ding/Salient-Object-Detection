00084e7cf4ef304c7d72a327fb48b42d505bdd0c
weitian-ding <wt2ding@uwaterloo.ca>
2018-07-19 14:57:15 -0400

    implemented inference for alov300++

--- inference_alov300++.py
+++ inference_alov300++.py
@@ -1,9 +1,13 @@
+from glob import glob
+from os import path
+
 import tensorflow as tf
 import numpy as np
 import os
 from scipy import misc
 import argparse
 import sys
+from skimage import color
 
 g_mean = np.array(([126.88,120.24,112.19])).reshape([1,1,3])
 output_folder = "./test_output"
@@ -12,9 +16,11 @@ def rgba2rgb(img):
 	return img[:,:,:3]*np.expand_dims(img[:,:,3],2)
 
 def main(args):
-	
+
+	output_folder = args.rgb_folder.repace('imagedata++', 'saliencymaps')
+
 	if not os.path.exists(output_folder):
-		os.mkdir(output_folder)	
+		os.makedirs(output_folder)
 	
 	gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction = args.gpu_fraction)
 	with tf.Session(config=tf.ConfigProto(gpu_options = gpu_options)) as sess:
@@ -24,18 +30,31 @@ def main(args):
 		pred_mattes = tf.get_collection('mask')[0]
 
 		if args.rgb_folder:
-			rgb_pths = os.listdir(args.rgb_folder)
+			rgb_pths = [y for x in os.walk(args.rgb_folder) for y in glob(os.path.join(x[0], '*.jpg'))]
 			for rgb_pth in rgb_pths:
-				rgb = misc.imread(os.path.join(args.rgb_folder,rgb_pth))
+				rgb = misc.imread(rgb_pth)
+
+				if len(rgb.shape) == 2:
+					rgb = color.gray2rgb(rgb)
 				if rgb.shape[2]==4:
 					rgb = rgba2rgb(rgb)
+
+				assert(rgb.dtype == np.ubyte)
+
 				origin_shape = rgb.shape
 				rgb = np.expand_dims(misc.imresize(rgb.astype(np.uint8),[320,320,3],interp="nearest").astype(np.float32)-g_mean,0)
 
 				feed_dict = {image_batch:rgb}
 				pred_alpha = sess.run(pred_mattes,feed_dict = feed_dict)
 				final_alpha = misc.imresize(np.squeeze(pred_alpha),origin_shape)
-				misc.imsave(os.path.join(output_folder,rgb_pth),final_alpha)
+
+				save_path = rgb_pth.replace('imagedata++', 'saliencymaps')
+				save_dir = path.dirname(save_path)
+
+				if not path.exists(save_dir):
+					os.makedirs(save_dir)
+
+				misc.imsave(save_path,final_alpha)
 
 		else:
 			rgb = misc.imread(args.rgb)
